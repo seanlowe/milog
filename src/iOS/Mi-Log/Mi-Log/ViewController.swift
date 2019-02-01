a894205fbf796a2ac306a6232e35e8fa950bdb7f
Britni Rogers <britnirogers@2600-6c67-237f-c2e7-00ce-57c5-d121-6323.res6.spectrum.com>
2019-01-31 22:01:20 -0700

    Created the initial project for iOS

---
+++ src/iOS/Mi-Log/Mi-Log/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mi-Log
+//
+//  Created by Britni Rogers on 1/31/19.
+//  Copyright © 2019 Mi-Log. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
