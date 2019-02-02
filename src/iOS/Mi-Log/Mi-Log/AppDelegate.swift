251fdb50811197669d449d7f44d95ad17a30966c
sonofmath <britnirogers@britnirsmacbook>
2019-02-01 21:28:52 -0700

    Connected the Firebase Realtime Database

--- src/iOS/Mi-Log/Mi-Log/AppDelegate.swift
+++ src/iOS/Mi-Log/Mi-Log/AppDelegate.swift
@@ -14,10 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    var ref: DatabaseReference!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        ref = Database.database().reference()
+        
         return true
     }
 
