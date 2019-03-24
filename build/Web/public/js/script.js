975a01ba4a3525ca2024ee6538528e8b367dd3c9
sonofmath <sonofmath3.14@hotmail.com>
2019-03-23 22:52:18 -0600

    Authentication working on new login

--- build/Web/public/js/script.js
+++ build/Web/public/js/script.js
@@ -1,8 +1,3 @@
-// require('dotenv').config()
-
-const status = {
-  
-}
 
 const config = {
   apiKey: "AIzaSyB_4KJ05TIv6G6sHUObbV91k2Q3qINw52c",
@@ -37,50 +32,7 @@ function writeUserData() {
     });
 }
 
-function toggleSignIn() {
-  if (firebase.auth().currentUser) {
-    // [START signout]
-    firebase.auth().signOut();
-    // [END signout]
-  } else {
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    if (email.length < 4) {
-      alert("Please enter an email address.");
-      return;
-    }
-    if (password.length < 4) {
-      alert("Please enter a password.");
-      return;
-    }
-    // Sign in with email and pass.
-    // [START authwithemail]
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // [START_EXCLUDE]
-        if (errorCode === "auth/wrong-password") {
-          alert("Wrong password.");
-        } else {
-          alert(errorMessage);
-        }
-        console.log(error);
-        document.getElementById("sign-in").disabled = false;
-        // [END_EXCLUDE]
-      });
-    // [END authwithemail]
-  }
-  document.getElementById("sign-in").disabled = true;
-}
-
-/**
- * Handles the sign up button press.
- */
-function handleSignUp() {
+function handleSignIn() {
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
   if (email.length < 4) {
@@ -92,6 +44,46 @@ function handleSignUp() {
     return;
   }
   // Sign in with email and pass.
+  // [START authwithemail]
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // [START_EXCLUDE]
+      if (errorCode === "auth/wrong-password") {
+        alert("Wrong password.");
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+      // [END_EXCLUDE]
+    });
+    // [END authwithemail]
+}
+
+function handleSignOut() {
+  firebase.auth().signOut();
+  window.location.href = "login.html";
+}
+
+/**
+ * Handles the sign up button press.
+ */
+function handleSignUp() {
+  var email = document.getElementById("signup-Email").value;
+  var password = document.getElementById("signup-Password").value;
+  if (email.length < 4) {
+    alert("Please enter an email address.");
+    return;
+  }
+  if (password.length < 4) {
+    alert("Please enter a password.");
+    return;
+  }
+  // Sign up with email and pass.
   // [START createwithemail]
   firebase
     .auth()
@@ -110,29 +102,23 @@ function handleSignUp() {
       // [END_EXCLUDE]
     });
   // [END createwithemail]
-  
 }
 
 /**
  * Sends an email verification to the user.
  */
 function sendEmailVerification() {
-  // [START sendemailverification]
-
   firebase
     .auth()
     .currentUser.sendEmailVerification()
     .then(function() {
       // Email Verification sent!
-      // [START_EXCLUDE]
       alert("Email Verification Sent!");
-      // [END_EXCLUDE]
     });
-  // [END sendemailverification]
 }
 
 function sendPasswordReset() {
-  var email = document.getElementById("email").value;
+  var email = document.getElementById("resetEmail").value;
   // [START sendpasswordemail]
   firebase
     .auth()
@@ -168,56 +154,37 @@ function initApp() {
   // Listening for auth state changes.
   // [START authstatelistener]
   firebase.auth().onAuthStateChanged(function(user) {
-    // [START_EXCLUDE silent]
-    document.getElementById("verify-email").disabled = true;
-    // [END_EXCLUDE]
-    if (user && user.emailVerified == true) {
+
+    if (user) {
       // User is signed in.
-      // window.location.href = "not-yet.html";
       var email = user.email;
       var emailVerified = user.emailVerified;
       var uid = user.uid;
-      // [START_EXCLUDE]
-      document.getElementById("sign-in-status").textContent =
-        "Signed in";
-      document.getElementById("sign-in").textContent = "Sign out";
-      document.getElementById(
-        "account-details"
-      ).textContent = JSON.stringify(user, null, "  ");
-      if (!emailVerified) {
-        document.getElementById("verify-email").disabled = false;
+      if (emailVerified == true){
+        window.location.href = "home.html";
+        document.getElementById("userEmail").textContent = email;
+        alert("Signed in with email: " + email);
+      } else {
+        window.location.href = "login.html";
+        sendEmailVerification();
+        alert("Please verify your email");
       }
-      // [END_EXCLUDE]
-    } else if (user && user.emailVerified == false) {
-      alert("Please verify your email");
-      document.getElementById("verify-email").disabled = false;
+  
     } else {
       // User is signed out.
-      // [START_EXCLUDE]
-      document.getElementById("sign-in-status").textContent =
-        "Signed out";
-      document.getElementById("sign-in").textContent = "Sign in";
-      document.getElementById("account-details").textContent =
-        "null";
-      // [END_EXCLUDE]
     }
-    // [START_EXCLUDE silent]
-    document.getElementById("sign-in").disabled = false;
-    // [END_EXCLUDE]
   });
   // [END authstatelistener]
+
   document
     .getElementById("sign-in")
-    .addEventListener("click", toggleSignIn, false);
-  // document
-  //   .getElementById("sign-out")
-  //   .addEventListener("click", handleSignOut, false);
+    .addEventListener("click", handleSignIn, false);
+  document
+    .getElementById("sign-out")
+    .addEventListener("click", handleSignOut, false);
   document
     .getElementById("sign-up")
     .addEventListener("click", handleSignUp, false);
-  document
-    .getElementById("verify-email")
-    .addEventListener("click", sendEmailVerification, false);
   document
     .getElementById("password-reset")
     .addEventListener("click", sendPasswordReset, false);
