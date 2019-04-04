da6b1727c633b6da286f49d9165b78aeff2bedb7
Sean Lowe <sslowe@mavs.coloradomesa.edu>
2019-04-04 10:03:51 -0600

    remove flutter blue stuff

--- flutter_blue/lib/gen/flutterblue.pbserver.dart
+++
@@ -1,8 +0,0 @@
-///
-//  Generated code. Do not modify.
-//  source: flutterblue.proto
-///
-// ignore_for_file: non_constant_identifier_names,library_prefixes,unused_import
-
-export 'flutterblue.pb.dart';
-
