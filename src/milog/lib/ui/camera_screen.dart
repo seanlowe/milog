7a7c8207f53d2c96b67c4aa0f4022d20e9205c39
Riad Shash <riadshash@yahoo.com>
2019-04-11 20:00:45 -0600

    Can now recognize text blocks - solved race condition problem

--- src/milog/lib/ui/camera_screen.dart
+++ src/milog/lib/ui/camera_screen.dart
@@ -4,7 +4,10 @@ import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:milog/main.dart';
+
+enum Detector { text }
 
 class CameraScreen extends StatefulWidget {
   @override
@@ -18,14 +21,18 @@ void logError(String code, String message) =>
 
 class _CameraScreenState extends State<CameraScreen> {
   CameraController controller;
-  String imagePath;
-  Image processImage;
-  File imageFile;
+  String _imagePath; //Path of the image that was taken
+  File _imageFile; //Image file from the imagePath
+  Size _imageSize; //The size of the image
+  VisionText _scanResults; //Results from scan
+  Detector _currentDetector = Detector.text; //Enum
+  bool working = false;
 
+  //Recognized text/numbers in an image
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     controller = CameraController(cameras[0], ResolutionPreset.medium);
     controller.initialize().then((_) {
@@ -36,6 +43,123 @@ class _CameraScreenState extends State<CameraScreen> {
     });
   }
 
+  Future<void> _getAndScanImage() async{
+    print("_getAndScanImage invoked");
+    setState(() {});
+
+    //Reference to a file
+     File imageFile = new File(_imagePath);
+
+    if (imageFile != null) {
+      await _getImageSize(imageFile);
+      await _scanImage(imageFile);
+    }
+    
+    setState(() {
+      _imageFile = imageFile;
+      findBestOdometerCandidate();
+    });
+  }
+
+  Future<void> _getImageSize(File imageFile) async {
+    print("_getImageSize invoked");
+    final Completer<Size> completer = Completer<Size>();
+
+    final Image image = Image.file(imageFile);
+    image.image.resolve(const ImageConfiguration()).addListener(
+      (ImageInfo info, bool _) {
+        completer.complete(Size(
+          info.image.width.toDouble(),
+          info.image.height.toDouble(),
+        ));
+      },
+    );
+
+    final Size imageSize = await completer.future;
+    setState(() {
+      _imageSize = imageSize;
+    });
+  }
+
+
+  //Generates a FirebaseVisionImage from image
+  Future<void> _scanImage(File imageFile) async {
+    setState(() {
+      _scanResults = null;
+    });
+
+    print("_scanImage is invoked");
+
+    final FirebaseVisionImage visionImage =
+        FirebaseVisionImage.fromFile(imageFile);
+    
+    print("visionImage right after getting it .fromFile is" + visionImage.toString());
+
+
+    dynamic results;
+    switch (_currentDetector) {
+      case Detector.text:
+        final TextRecognizer recognizer =
+            FirebaseVision.instance.textRecognizer();
+        results = await recognizer.processImage(visionImage);
+        print("results in _scanImage = " + results.toString());
+        break;
+      default:
+        return;
+    }
+
+    setState(() {
+      _scanResults = results;
+    });
+  }
+
+  void findBestOdometerCandidate() {
+    //Recognized text/numbers in an image
+    final VisionText visionText = _scanResults;
+    print("in findBestOdometerCandidate, _scanResults = " + _scanResults.toString());
+
+    String strDect = "";
+    int odoReading = 0;
+
+    //Finds the best odometer candidate
+    int odoCandidate(String inputStr) {
+      String strResult = "";
+      int intCode = 0;
+
+      //Check to see if they are all digits
+      for (int i = 0; i < inputStr.length; i++) {
+        intCode = inputStr.codeUnitAt(i);
+        if (intCode >= 48 && intCode <= 57) {
+          strResult += String.fromCharCode(intCode);
+        } else {
+          strResult = "";
+          break;
+        }
+      }
+      if (strResult.isNotEmpty)
+        return int.parse(strResult);
+      else
+        return 0;
+    }
+
+    print("Finding text/numbers in image...");
+    if (visionText != null) {
+      for (TextBlock block in visionText.blocks) {
+        for (TextLine line in block.lines) {
+          for (TextElement element in line.elements) {
+            strDect = element.text.toString();
+
+            print("Detected: " + strDect);
+            
+            //showInSnackBar("Detected: " + strDect.toString());
+
+          }
+        }
+      }
+    }else{
+      print("visionTest is NULL!!!");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +175,7 @@ class _CameraScreenState extends State<CameraScreen> {
               child: Padding(
                 padding: const EdgeInsets.all(1.0),
                 child: Center(
-                  child: _cameraPreviewWidget(),
+                  child: (!working) ? _cameraPreviewWidget() : _showCircularProgress(working),
                 ),
               ),
               decoration: BoxDecoration(
@@ -71,7 +195,7 @@ class _CameraScreenState extends State<CameraScreen> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-               // _cameraTogglesRowWidget(),
+                // _cameraTogglesRowWidget(),
               ],
             ),
           ),
@@ -99,7 +223,7 @@ class _CameraScreenState extends State<CameraScreen> {
     }
   }
 
-  /// Display the control bar with buttons to take pictures and record videos.
+  /// Display the control bar with buttons to take pictures
   Widget _captureControlRowWidget() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -107,15 +231,26 @@ class _CameraScreenState extends State<CameraScreen> {
       children: <Widget>[
         IconButton(
           icon: const Icon(Icons.camera_alt),
-          color: Colors.blue,
-          onPressed: controller != null &&
-                  controller.value.isInitialized &&
-                  !controller.value.isRecordingVideo
-              ? onTakePictureButtonPressed
-              : null,
+          color: Colors.red,
+          onPressed: takePictureAndProcess,
         ),
       ],
     );
+  }
+
+  Widget _showCircularProgress(isWorking){
+    if (isWorking) {
+      return Center(child: CircularProgressIndicator());
+    } return Container(height: 0.0, width: 0.0,);
+  }
+
+
+  void takePictureAndProcess() {
+    if (controller != null &&
+        controller.value.isInitialized &&
+        !controller.value.isRecordingVideo) {
+      onTakePictureButtonPressed();
+    }
   }
 
   String timestamp() => DateTime.now().millisecondsSinceEpoch.toString();
@@ -128,30 +263,33 @@ class _CameraScreenState extends State<CameraScreen> {
     takePicture().then((String filePath) {
       if (mounted) {
         setState(() {
-          imagePath = filePath;
-          imageFile = new File(filePath);
-          processImage = Image.file(imageFile);
+          _imagePath = filePath;
+          print("in onTakePictureButtonPressed, imagePath = " +
+              _imagePath.toString());
+          //Gets the image file from the filePath of the image
+          //_imageFile = new File(filePath);
+          //print("This is the file form the filePath: " + _imageFile.toString());
         });
-        if (filePath != null) showInSnackBar('Picture saved to $filePath');
+        if (filePath != null) {
+          //showInSnackBar('Picture saved to $filePath');
+          _getAndScanImage();
+        } else {
+          showInSnackBar("I don't have the image!");
+        }
       }
     });
   }
 
   Future<String> takePicture() async {
-    if (!controller.value.isInitialized) {
-      showInSnackBar('Error: select a camera first.');
-      return null;
-    }
     final Directory extDir = await getApplicationDocumentsDirectory();
     final String dirPath = '${extDir.path}/Pictures/flutter_test';
     await Directory(dirPath).create(recursive: true);
     final String filePath = '$dirPath/${timestamp()}.jpg';
 
     if (controller.value.isTakingPicture) {
-      // A capture is already pending, do nothing.
       return null;
     }
-
+   
     try {
       await controller.takePicture(filePath);
     } on CameraException catch (e) {
@@ -175,7 +313,6 @@ class CameraApp extends StatelessWidget {
     );
   }
 }
-
 
 //List<CameraDescription> cameras;
 
