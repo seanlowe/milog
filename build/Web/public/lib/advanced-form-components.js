124e02f68bef10b29d5b5fdc2e9b03b94e6bf55f
sonofmath <sonofmath3.14@hotmail.com>
2019-04-15 11:08:00 -0600

    Small modification to datepicker

--- build/Web/public/lib/advanced-form-components.js
+++ build/Web/public/lib/advanced-form-components.js
@@ -12,30 +12,30 @@
         $('.dpMonths').datepicker();
 
 
-        var startDate = new Date(2012,1,20);
-        var endDate = new Date(2012,1,25);
-        $('.dp4').datepicker()
-            .on('changeDate', function(ev){
-                if (ev.date.valueOf() > endDate.valueOf()){
-                    $('.alert').show().find('strong').text('The start date can not be greater then the end date');
-                } else {
-                    $('.alert').hide();
-                    startDate = new Date(ev.date);
-                    $('#startDate').text($('.dp4').data('date'));
-                }
-                $('.dp4').datepicker('hide');
-            });
-        $('.dp5').datepicker()
-            .on('changeDate', function(ev){
-                if (ev.date.valueOf() < startDate.valueOf()){
-                    $('.alert').show().find('strong').text('The end date can not be less then the start date');
-                } else {
-                    $('.alert').hide();
-                    endDate = new Date(ev.date);
-                    $('.endDate').text($('.dp5').data('date'));
-                }
-                $('.dp5').datepicker('hide');
-            });
+        // var startDate = new Date(2012,1,20);
+        // var endDate = new Date(2012,1,25);
+        // $('.dp4').datepicker()
+        //     .on('changeDate', function(ev){
+        //         if (ev.date.valueOf() > endDate.valueOf()){
+        //             $('.alert').show().find('strong').text('The start date can not be greater then the end date');
+        //         } else {
+        //             $('.alert').hide();
+        //             startDate = new Date(ev.date);
+        //             $('#startDate').text($('.dp4').data('date'));
+        //         }
+        //         $('.dp4').datepicker('hide');
+        //     });
+        // $('.dp5').datepicker()
+        //     .on('changeDate', function(ev){
+        //         if (ev.date.valueOf() < startDate.valueOf()){
+        //             $('.alert').show().find('strong').text('The end date can not be less then the start date');
+        //         } else {
+        //             $('.alert').hide();
+        //             endDate = new Date(ev.date);
+        //             $('.endDate').text($('.dp5').data('date'));
+        //         }
+        //         $('.dp5').datepicker('hide');
+        //     });
 
         // disabling dates
         var nowTemp = new Date();
@@ -43,11 +43,11 @@
 
         var checkin = $('.dpd1').datepicker({
             onRender: function(date) {
-                return date.valueOf() < now.valueOf() ? 'disabled' : '';
+                return date.valueOf() > now.valueOf() ? 'disabled' : '';
             }
         })
         .on('changeDate', function(ev) {
-                if (ev.date.valueOf() > checkout.date.valueOf()) {
+                if (ev.date.valueOf() < checkout.date.valueOf()) {
                     var newDate = new Date(ev.date)
                     newDate.setDate(newDate.getDate() + 1);
                     checkout.setValue(newDate);
@@ -56,8 +56,9 @@
                 $('.dpd2')[0].focus();
             }).data('datepicker');
         var checkout = $('.dpd2').datepicker({
+            endDate: now,
             onRender: function(date) {
-                return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+                return (date.valueOf() < now.valueOf()) && (date.valueOf() >= checkin.date.valueOf()) ? '' : 'disabled';
             }
         }).on('changeDate', function(ev) {
                 checkout.hide();
